b19ce94e60c1de7a1d674bc289c0fd9d77926a63
ShrivKartik <codewithkartikey2003@gmail.com>
2023-03-04 22:20:02 +0530

    Count digit in a number

---
+++ CountDigit.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int countDigit(int n){
+    int count=0;
+
+    while(n>0){
+        n/=10;
+        count++;
+    }
+    return count;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    cout<<countDigit(n)<<endl;
+    return 0;
+}
