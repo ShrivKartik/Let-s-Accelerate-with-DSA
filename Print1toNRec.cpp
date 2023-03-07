791db1c15a155416b0afdfc1d86d7180834736d4
ShrivKartik <codewithkartikey2003@gmail.com>
2023-03-07 23:20:29 +0530

    Print 1 to N using Recursion

---
+++ Print1toNRec.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void print1ToN(int n){
+    if(n==0)
+    return ;
+
+    print1ToN(n-1);
+    cout<<n<<" ";
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    print1ToN(n);
+}
