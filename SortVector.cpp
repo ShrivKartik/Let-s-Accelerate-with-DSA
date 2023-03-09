c5f241c350a4f43d3ecb8af6cd24093345859c5a
ShrivKartik <codewithkartikey2003@gmail.com>
2023-03-09 16:30:03 +0530

    Sorting a vector

---
+++ SortVector.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> v = {5,7,20,10};
+
+    sort(v.begin(), v.end());
+
+    for(int x:v){
+        cout<<x<<" ";
+    }
+}
